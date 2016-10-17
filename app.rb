ec29a6af4d588f8e6b746eef3df4b56a2f7a5f56
jameshamann <jameshamann@me.com>
2016-10-17 11:31:02 +0100

    server now runs on localhost:4000

--- app.rb
+++ app.rb
@@ -1,10 +1,17 @@
 require 'sinatra/base'
 
+
 class DbTechTest < Sinatra::Base
+
+  set :port, 4000
+
   get '/' do
     'Hello DbTechTest!'
   end
 
+
+
   # start the server if ruby file executed directly
+  set :port, 4000
   run! if app_file == $0
 end
