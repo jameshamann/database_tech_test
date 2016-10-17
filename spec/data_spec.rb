48a235688fa4ed9f0d0c7af7f91804bc2760d9cf
jameshamann <jameshamann@me.com>
2016-10-17 12:06:00 +0100

    adds test to ensure data is stored in format of a hash

--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -4,9 +4,14 @@ describe DataStorage do
 
   subject(:data_storage) {described_class.new}
 
-  it 'should store values that are passed in within a hash' do
+  it 'should store values that are passed in as an argument' do
     data_storage.store('key','value')
     expect(data_storage.data).not_to be_empty
+  end
+
+  it 'should store values that are passed in within a hash' do
+    data_storage.store('key','value')
+    expect(data_storage.data).to eq(:key => 'value')
   end
 
 end
